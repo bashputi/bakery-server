cc1cd507e11ccaef209608d5fe4ec6178e04e277
Rime <rimeislam672@gmail.com>
2023-11-06 13:23:32 +0600

    get data to client site

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const cookieParser= require('cookie-parser');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+// const jwt = require('jsonwebtoken');
+// const cookieParser= require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5001;
 
-app.use(cors({
-    origin: ['http://localhost:5173'],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
+
+const port = process.env.PORT || 5000;
+
+// app.use(cors({
+//     origin: ['http://localhost:5173'],
+//   credentials: true,
+// }));
+
+// app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fobkzbd.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,22 +34,29 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-//    await client.connect();
+   await client.connect();
     const bakeryCollection = client.db('bakeryDB').collection('bakery');
-    
+    // post all data in server 
     app.post('/bakery', async(req, res) => {
         try {
-            const body = re.body;
+            const body = req.body;
             const result = await bakeryCollection.insertOne(body);
             res.send(result);
         } catch (error) {
             console.log(error)
         }
     });
-    
+    // get all data from server 
     app.get('/bakery', async(req, res) => {
         const result = await bakeryCollection.find().toArray();
         res.send(result);
+    })
+    // get single data 
+    app.get('/bakery/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await bakeryCollection.findOne(query);
+      res.send(result);
     })
 
     await client.db("admin").command({ ping: 1 });

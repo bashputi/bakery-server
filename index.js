44ed155b7fd9a3dcc1f783d031ece72ccdeef75b
Rime <rimeislam672@gmail.com>
2023-11-07 14:22:01 +0600

    client added item delete

--- index.js
+++ index.js
@@ -152,6 +152,12 @@ async function run() {
     const cursor = await myCollection.find().toArray();
     res.send(cursor);
   })
+  app.delete('/item/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await myCollection.deleteOne(query);
+    res.send(result);
+  })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
